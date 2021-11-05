5966cc155ae0b08eb37a2f87ba220604a67da6d8
nanko89 <natali.v.angelova@gmail.com>
2021-11-06 00:05:50 +0200

    Java OOP Lab

---
+++ InterfacesAndAbstraction/CarShop/Seat.java
@@ -0,0 +1,41 @@
+package CarShop;
+
+public class Seat implements Car {
+
+    private final String model;
+    private final String color;
+    private final int horsePower;
+    private final String countryProduced;
+
+    public Seat(String model, String color, int horsePower, String countryProduced) {
+        this.model = model;
+        this.color = color;
+        this.horsePower = horsePower;
+        this.countryProduced = countryProduced;
+    }
+
+    @Override
+    public String getModel() {
+        return this.model;
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+
+    @Override
+    public String countryProduced() {
+        return this.countryProduced;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("This is %s produced in %s and have %d tires", model, countryProduced, Car.TIRES);
+    }
+}
