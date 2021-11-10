17a11cc88b9f141cf29ad0667a328967f9f600cf
nanko89 <natali.v.angelova@gmail.com>
2021-11-10 16:20:27 +0200

    Java OOP Lab

---
+++ Polymorphism/MathOperation/MathOperation.java
@@ -0,0 +1,15 @@
+package MathOperation;
+
+public class MathOperation {
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c){
+        return add(add( a ,b ), c);
+    }
+
+    public static int add(int a, int b, int c, int d){
+        return add(add( a ,b ), c, d);
+    }
+}
