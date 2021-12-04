427f3492ac2a6c834873a65210bdb32af5252f94
nanko89 <natali.v.angelova@gmail.com>
2021-12-04 17:44:30 +0200

    Java OOP Exam

--- ExamPreparation/Exam - 22 August 2021/farmville/FarmvilleTests.java
+++ ExamPreparation/Exam - 22 August 2021/farmville/FarmvilleTests.java
@@ -4,88 +4,77 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FarmvilleTests {
-    private List<Animal> animals;
+    private Farm farm;
     private Animal animal1;
     private Animal animal2;
-    private Animal animal3;
-    private Farm farm;
 
     @Before
     public void set(){
-        animal1 = new Animal("Cow", 10.0);
-        animal2 = new Animal("Sheep", 5.0);
-        animal3 = new Animal("Cow", 8.0);
-        farm = new Farm("MyFarm", 2);
-        animals = new ArrayList<>();
-    }
-
-
-    @Test(expected = NullPointerException.class)
-    public void testFarmNameEqualsNull(){
-        farm = new Farm(null, 5);
+        this.animal1 = new Animal("cow", 20);
+        this.animal2 = new Animal("chicken", 5);
+        this.farm = new Farm("Farm", 10);
     }
 
     @Test(expected = NullPointerException.class)
-    public void testFarmNameIsEmpty(){
-        farm = new Farm("    ", 5);
+    public void setNullForNamePerFarm(){
+        Farm farm = new Farm(null, 10);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testFarmSetNullCapacity(){
-        farm = new Farm("Farm" , -1);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testAnimalSizeWhenIsEqualsFarmCapacity(){
-        farm.add(animal1);
-        farm.add(animal2);
-        farm.add(animal3);
+    @Test(expected = NullPointerException.class)
+    public void setEmptyNamePerFarm(){
+        Farm farm = new Farm("   ", 10);
     }
 
     @Test
-    public void testAddAnimalInFarm(){
-        farm.add(animal2);
+    public void setCorrectNamePerFarm(){
+        Assert.assertEquals("Farm", farm.getName());
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testAnimalSizeWhenIsEqualsAnimalType(){
-       farm.add(animal1);
-       farm.add(animal3);
+    public void testSetZeroCapacity(){
+        Farm farm = new Farm("New", -1);
     }
 
     @Test
-    public void testRemoveAnimalFromFarm(){
+    public void testSetCorrectCapacity(){
+        Assert.assertEquals(10, farm.getCapacity());
+    }
+
+    @Test
+    public void testRemoveAnimalReturnTrue(){
         farm.add(animal1);
+        farm.add(animal2);
         Assert.assertTrue(farm.remove(animal1.getType()));
-        Assert.assertFalse(farm.remove(animal1.getType()));
     }
 
     @Test
-    public void testGetAnimalsCount(){
+    public void testRemoveAnimalReturnFalse(){
         farm.add(animal1);
-        Assert.assertEquals(farm.getCount(), 1);
+        farm.add(animal2);
+        Assert.assertFalse(farm.remove("dog"));
     }
 
-    @Test
-    public void testFarmName(){
-        Assert.assertEquals(farm.getName(), "MyFarm");
-    }
-
-    @Test
-    public void testFarmCount(){
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddMethodWhenCapacityIsFull(){
+        Farm farm = new Farm("Farm", 1);
         farm.add(animal1);
-        Assert.assertEquals(1,farm.getCount() );
+        farm.add(animal2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddMethodWhenAnimalExist(){
+        farm.add(animal1);
+        farm.add(animal1);
     }
 
     @Test
-    public void testGetEnergyAnimal(){
-        animals.add(animal1);
-        Assert.assertEquals(10.0, animal1.getEnergy(), 0.0);
+    public void testCorrectAddAnimal(){
+        farm.add(animal1);
+        farm.add(animal2);
+        Assert.assertEquals(2, farm.getCount());
     }
 
+    
 
 }
